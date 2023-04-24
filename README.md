b2b6661dff35973ccbaadf880d5d671816197bad
alexanderjac <alexander.jac@northeastern.edu>
2023-04-24 13:39:11 -0400

    Fresh commit - added methods to load three datasets to mysql database

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
 # CommentDB_Assessment
+
+# Solution Class ReadMe
+
+The Solution class is a Python class that provides methods for loading data from CSV and JSON files into a MySQL database. The class takes in connection details for the MySQL database (host, user, password, and database name) as input during initialization.
+
+# Dependencies
+The following libraries are required to use the Solution class:
+
+json
+mysql.connector
+os
+pandas
+re
+datetime
+pytz
+Usage
+## Initialization
+To use the Solution class, you need to create an instance of the class by passing in the connection details for the MySQL database during initialization, like this:
+
+python
+solution = Solution(host="localhost", user="root", password="password", database="mydb")
+
+Make sure to replace the values for host, user, password, and database with the appropriate values for your MySQL database.
+
+## Methods
+The Solution class has the following methods:
+
+load_comment_text
+The load_comment_text method allows you to load data from CSV files into the MySQL database. You need to pass in the path to the directory containing the CSV files as an argument, like this:
+
+python
+solution.load_comment_text(comment_txt_path="/path/to/csv/files")
+
+The method will create a table called "comment_text" in the MySQL database (if it doesn't already exist), and then load the data from the CSV files into this table.
+
+load_comment_info
+The load_comment_info method allows you to load data from JSON files into the MySQL database. You need to pass in the path to the directory containing the JSON files as an argument, like this:
+
+python
+solution.load_comment_info(folder_path="/path/to/json/files")
+
+The method will create a table called "comment_info" in the MySQL database (if it doesn't already exist), and then load the data from the JSON files into this table.
+
+## create_db_connection
+The create_db_connection method creates a MySQL database connection and returns a mysql.connector.connection.MySQLConnection object. You can use this method to establish a connection to the MySQL database if you need to perform any other operations that are not provided by the Solution class.
+
+create_table
+The create_table method allows you to create a table in the MySQL database. You need to pass in the table name and the column definitions as arguments. The column definitions should be provided as a list of tuples, where each tuple contains the column name and the data type, like this:
+
+python
+columns = [("column1", "data_type1"), ("column2", "data_type2"), ...]
+solution.create_table(table_name="mytable", columns=columns)
+
+This method is used internally by the load_comment_text and load_comment_info methods to create tables in the MySQL database for loading data from CSV and JSON files.
+
+# Note
+The Solution class assumes that the CSV files have two columns named "h_id" and "message", and the JSON files have a specific structure with "h_id", "posts", and "comments" fields. If your data has a different structure, you may need to modify the code accordingly.
+
+Also, please make sure to handle any exceptions or errors that may occur during the execution of the methods, such as invalid file paths, missing columns, or database connection errors, to ensure robustness of your application.
---
+++ solution.py
@@ -0,0 +1,177 @@
+import json
+import mysql.connector
+import os
+import pandas as pd
+import re
+from datetime import datetime
+import pytz
+
+
+class Solution:
+    def __init__(self, host, user, password, database):
+        self.host = host
+        self.user = user
+        self.password = password
+        self.database = database
+
+    def create_db_connection(self):
+        """
+        Create a MySQL database connection.
+
+        :return: mysql.connector.connection.MySQLConnection object
+        """
+        # Connect to MySQL database
+        conn = mysql.connector.connect(
+            host=self.host,
+            user=self.user,
+            password=self.password,
+            database=self.database
+        )
+        return conn
+    def create_table(self, table_name, columns):
+    # Connect to SQLite database
+        conn = self.create_db_connection()
+        cursor = conn.cursor()
+        column_defs = []
+        
+        for col in columns:
+            column_name, data_type = col[0], col[1]
+            column_defs.append(f"{column_name} {data_type}")
+        column_defs_str = ", ".join(column_defs)
+        cursor.execute(f"CREATE TABLE IF NOT EXISTS {table_name} ({column_defs_str})")
+        conn.commit()
+        conn.close()
+
+
+    def load_comment_text(self, comment_txt_path):
+        """
+        Load CSV files from a directory into MySQL database.
+
+        :param comment_txt_path: str, path to the directory containing CSV files
+        """
+
+        # cursor = self.conn.cursor()
+        columns = [("h_id", "LONGTEXT"), ("message", "LONGTEXT")]
+        table_name = "comment_text"
+        self.create_table(table_name, columns)
+        conn = self.create_db_connection()
+        cursor = conn.cursor()
+        for file in os.listdir(comment_txt_path):
+            if file.endswith(".csv"):
+                file_path = os.path.join(comment_txt_path, file)
+
+                # Read CSV file into a pandas DataFrame
+                df = pd.read_csv(file_path,encoding='utf-8')
+
+                # Load DataFrame into MySQL
+                df.apply(lambda row: cursor.execute("INSERT INTO {} (h_id, message) VALUES (%s, %s)".format(table_name), (row['h_id'], row['message'])), axis=1)
+
+                # Commit the transaction
+                conn.commit()
+        cursor.close()
+        conn.close()
+    
+
+    def load_comment_info(self, folder_path):
+        # Create an empty DataFrame to store the normalized data
+        df_combined = pd.DataFrame()
+
+        for file_name in os.listdir(folder_path):
+            if file_name.endswith(".json"):
+                file_path = os.path.join(folder_path, file_name)
+                with open(file_path, 'r') as file:
+                    for line in file:
+                        try:
+                            data = json.loads(line)
+                            # Extract relevant fields from the JSON object
+                            post_id = data["h_id"]
+                            if "posts" in data:
+                                posts = data["posts"]["data"]
+                                for post in posts:
+                                    post_data = {"post_id": post["h_id"],
+                                                "created_time": post.get("created_time", datetime(1970, 1, 16, 0, 51, 52, tzinfo=pytz.utc)),
+                                                "like_count": max(post.get("up_likes", 0), post.get("like_count", 0)),
+                                                "comment_count": post.get("comment_count", 0)}
+                                    # Normalize comments data and append to combined DataFrame
+                                    if "comments" in post:
+                                        comments = post["comments"]["data"]
+                                        for comment in comments:
+                                            comment_data = {"post_id": post["h_id"],
+                                                            "comment_id": comment["h_id"],
+                                                            "created_time": comment.get("created_time", datetime(1970, 1, 16, 0, 51, 52, tzinfo=pytz.utc)),
+                                                            "like_count": max(comment.get("up_likes", 0), comment.get("like_count", 0)),
+                                                            "comment_count": comment.get("comment_count", 0)}
+                                            df_combined = pd.concat([df_combined, pd.json_normalize(comment_data)], ignore_index=True)
+                                    df_combined = pd.concat([df_combined, pd.json_normalize(post_data)], ignore_index=True)
+                            else:
+                                df_combined = pd.concat([df_combined, pd.json_normalize({"post_id": post_id,
+                                                                                    "created_time": datetime(1970, 1, 16, 0, 51, 52, tzinfo=pytz.utc),
+                                                                                    "like_count": 0,
+                                                                                    "comment_count": 0})], ignore_index=True)
+                        except json.JSONDecodeError:
+                            print(f"Error parsing JSON in file: {file_path}")
+                            continue
+
+        df_combined['created_time'] = pd.to_datetime(df_combined['created_time'])  # Convert created_time column to datetime data type
+
+        # Define column names and data types for the database table
+        columns = [("post_id", "LONGTEXT"), ("comment_id", "LONGTEXT"),("created_time", "DATETIME"),("like_count", "INT"),("comment_count", "INT")]
+        table_name = "comment_info"
+        self.create_table(table_name, columns)  # Create the table in the database
+        conn = self.create_db_connection()
+        cursor = conn.cursor()
+
+        df_combined['created_time'] = df_combined['created_time'].apply(lambda x: x.strftime('%Y-%m-%d %H:%M:%S')) # Convert created_time to string representation of timestamp
+
+        # Insert data into the database table
+        df_combined.apply(lambda row: cursor.execute("INSERT INTO {} (post_id, comment_id, created_time, like_count, comment_count) VALUES (%s, %s, %s, %s, %s)".format(table_name), (row['post_id'], row['comment_id'], row['created_time'], row['like_count'], row['comment_count'])), axis=1)
+        conn.commit()
+        cursor.close()
+        conn.close()
+
+    def load_post_meta(self, folder_path):
+        columns = [ ("created_time", "DATETIME"), ("type", "VARCHAR(5)"), ("post_h_id", "LONGTEXT"),("page_h_id", "LONGTEXT"),("name_h_id", "LONGTEXT")]
+        table_name = "post_meta"
+        self.create_table(table_name, columns)
+        files = os.listdir(folder_path)
+        parquet_files = [pos_json for pos_json in os.listdir(folder_path) if pos_json.endswith('.parquet')]
+        merged_df = pd.DataFrame()
+        for file in parquet_files:
+                # Create the file path by joining the folder path and file name
+                file_path = os.path.join(folder_path, file)
+
+                # Read the Parquet file into a temporary dataframe
+                temp_df = pd.read_parquet(file_path)
+
+                # Concatenate the temporary dataframe with the merged dataframe
+                merged_df = pd.concat([merged_df, temp_df], ignore_index=True)
+        merged_df['created_time'] = pd.to_datetime(merged_df['created_time'])  # Convert created_time column to datetime data type
+
+        conn = self.create_db_connection()
+        cursor = conn.cursor()
+
+        for index, row in merged_df.iterrows():
+            created_time = row["created_time"].strftime('%Y-%m-%d %H:%M:%S')  # Convert timestamp to string format
+            cursor.execute(f"INSERT INTO {table_name} (created_time, type, post_h_id, page_h_id, name_h_id) "
+                        f"VALUES (%s, %s, %s, %s, %s)",
+                        (created_time, row["type"], row["post_h_id"], row["page_h_id"], row["name_h_id"]))
+
+        conn.commit()
+        conn.close()
+
+if __name__ == "__main__":
+    comment_txt_path ='/Users/jacksonalexander/Documents/COOP & Internships/Interview logs/Paramount/Assignment/Files/comment_text'
+    comment_info_path ='/Users/jacksonalexander/Documents/COOP & Internships/Interview logs/Paramount/Assignment/Files/comment_info_jsonl'
+    post_meta_path ='/Users/jacksonalexander/Documents/COOP & Internships/Interview logs/Paramount/Assignment/Files/post_meta'
+    host='Jacksons-MacBook-Pro.local'
+    user='root'
+    password='paste_password_here'
+    database='CommentDB'
+    sol = Solution(host, user, password, database)
+
+    # Load comment text data into MySQL
+    sol.load_comment_text(comment_txt_path)
+     # Load comment info data into MySQL
+    sol.load_comment_info(comment_info_path)
+    # Load post meta data into MySQL
+    sol.load_post_meta(post_meta_path)
